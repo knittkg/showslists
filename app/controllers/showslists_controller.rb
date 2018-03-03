6f8047c1008a78856d9354e7c6a25164ddd8dae3
knit tkg <ktakagi@nttpc.co.jp>
2018-03-03 12:51:14 +0000

    modifiedcontroller

--- app/controllers/showslists_controller.rb
+++ app/controllers/showslists_controller.rb
@@ -66,8 +66,9 @@ class ShowslistsController < ApplicationController
   def download
     @upload_file = Showslist.find(params[:id].to_i)
     filepath = @upload_file.filename.url
-    stat = File::stat(filepath)
-    send_file(filepath, :filename => @upload_file.filename.url.gsub(/.*\//,''), :length => stat.size)
+    # stat = File::stat(filepath)
+    # send_file(filepath, :filename => @upload_file.filename.url.gsub(/.*\//,''), :length => stat.size)
+    send_file(filepath, :filename => @upload_file.filename.url.gsub(/.*\//,''))
   end
 
   private
