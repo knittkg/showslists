a3dcc17e067ae7f6449b13cc710ea5362ef90fff
knit tkg <ktakagi@nttpc.co.jp>
2018-03-12 12:34:08 +0000

    201803122130 modified get_audio_job

--- app/jobs/get_audio_job.rb
+++ app/jobs/get_audio_job.rb
@@ -42,8 +42,27 @@ class GetAudioJob < ApplicationJob
 
         # 保存したid3情報を読み出してファイル名とともにshowslistテーブルに新規登録する処理
           AudioInfo.open(filename) do |info|
+            # titleを読み出して演奏日、演奏した都道府県、演奏した箱に分割
+            title = info.title
+            puts title
+            septitle = title.split(",")
+            p septitle 
+            livedate = septitle[2]
+            puts livedate
+            livepref = septitle[1]
+            puts livepref
+            liveplace_tmp1 = septitle[0]
+            puts liveplace_tmp1
+            liveplace_ary = liveplace_tmp1.split(" ")
+            p liveplace_ary
+            liveplace_tmp2 = []
+            liveplace_tmp2 << liveplace_ary[2,3]
+            puts liveplace_tmp2 
+            liveplace = liveplace_tmp2.join
+            puts liveplace
+
             stat = File.stat(filename)
-            Showslist.create( filename: fixture_file_upload(filename, 'audio/mpeg') , name: info.artist , title: info.title , length: stat.size , playtime: info.length)
+            Showslist.create( filename: fixture_file_upload(filename, 'audio/mpeg') , live_date: livedate , live_place: liveplace , live_pref: livepref , name: info.artist , title: info.title , length: stat.size , playtime: info.length)
             puts 'recentshow.mp3 new entry created'
           end
         # 管理者宛に更新通知のメール送信
