38c7e3a010633a98745560658bccbd5a0c328d48
knit tkg <ktakagi@nttpc.co.jp>
2018-03-18 15:02:15 +0000

    201803190000 Modified Showslists Validations

--- app/models/showslist.rb
+++ app/models/showslist.rb
@@ -1,11 +1,13 @@
 class Showslist < ApplicationRecord
   mount_uploader :filename, AudioUploader
   validates :live_date, length: { is: 8 }
+  validates :length, presence: true, length: { minimum: 1 }
+  validates :playtime, presence: true, length: { minimum: 1 }
   def self.search(search) #self.でクラスメソッドとしている
     if search # Controllerから渡されたパラメータが!= nilの場合は、カラムを部分一致検索
-      Showslist.where(['title LIKE ?', "%#{search}%"])
+      Showslist.where(['title LIKE ?', "%#{search}%"]).order(live_date: :DESC)
     else
-      Showslist.all #全て表示。
+      Showslist.order(live_date: :DESC) #全レコードを演奏日降順表示。
     end
   end
 end
